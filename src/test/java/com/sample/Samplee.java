a66f6c07796e6907f567497431f9897e5866b73b
sahusantoshirani-code <sahusantoshirani@gmail.com>
2026-06-11 11:03:54 +0530

    modifucation done by tester2

--- src/test/java/com/sample/Samplee.java
+++ src/test/java/com/sample/Samplee.java
@@ -1,12 +1,12 @@
 package com.sample;
 
 public class Samplee {
-	private void login() {
+	private void loginSerialNum() {
 		// TODO Auto-generated method stub
-System.out.println("******************");
+System.out.println("theb num is 7899");
 	}
-	private void loginId() {
-		System.out.println("Login id :***********");
+	private void loginIdProMax() {
+		System.out.println("Login id :123*****456******454655");
 
 	}
  public static void main(String[] args) {
