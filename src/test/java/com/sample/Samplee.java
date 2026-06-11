1e9672305bf736695b68b49d307e6fb545ce9c77
sahusantoshirani-code <sahusantoshirani@gmail.com>
2026-06-11 10:23:35 +0530

    added code

---
+++ src/test/java/com/sample/Samplee.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Samplee {
+	private void login() {
+		// TODO Auto-generated method stub
+System.out.println("******************");
+	}
+	private void loginId() {
+		System.out.println("Login id :***********");
+
+	}
+ public static void main(String[] args) {
+	
+}
+ 
+}
